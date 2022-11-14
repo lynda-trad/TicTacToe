2dc89e07b643d4dae513714a2aa949dbbf5ce766
lynda-trad <91526400+lynda-trad@users.noreply.github.com>
2022-11-14 15:56:48 +0100

    new functions in Game class : setWinner and setCurrentPlayer + changed Game attribute PlayerOne to currentPlayer

--- Game.java
+++ Game.java
@@ -2,16 +2,26 @@ package src;
 
 public class Game
 {
-	private boolean PlayerOne; /*Red == PlayerOne, Blue == PLayerTwo*/
+	private int currentPlayer; /*Red == Player One, Blue == Player Two*/
 	private int[][] Board;
 	private MyJFrame frame;
 	private int winner = 0;
 	
 	public Game()
 	{
-		PlayerOne = true;
+		currentPlayer = 1;
 		Board = new int[3][3];
 		frame = new MyJFrame();
+	}
+
+	private void setCurrentPlayer(int p)
+	{
+		this.currentPlayer = p;
+	}
+	
+	private void setWinner(int w)
+	{
+		this.winner = w;
 	}
 	
 	private static void checkWinner(Game g)
@@ -19,37 +29,37 @@ public class Game
 		// Diagonal
 		if(g.Board[0][0] == g.Board[1][1] && g.Board[1][1] == g.Board[2][2] && g.Board[0][0] != 0)
 		{
-			g.winner = g.Board[0][0];
+			g.setWinner(g.Board[0][0]);
 		}
 		else if(g.Board[0][2] == g.Board[1][1] && g.Board[1][1] == g.Board[2][0] && g.Board[0][2] != 0)
 		{
-			g.winner = g.Board[0][2];
+			g.setWinner(g.Board[0][2]);
 		}
 		// Vertical
 		else if(g.Board[0][0] == g.Board[1][0] && g.Board[1][0] == g.Board[2][0] && g.Board[0][0] != 0)
 		{
-			g.winner = g.Board[0][0];
+			g.setWinner(g.Board[0][0]);
 		}
 		else if(g.Board[0][1] == g.Board[1][1] && g.Board[1][1] == g.Board[2][1] && g.Board[0][1] != 0)
 		{
-			g.winner = g.Board[0][1];
+			g.setWinner(g.Board[0][1]);
 		}
 		else if(g.Board[0][2] == g.Board[1][2] && g.Board[1][2] == g.Board[2][2] && g.Board[0][2] != 0)
 		{
-			g.winner = g.Board[0][2];
+			g.setWinner(g.Board[0][2]);
 		}
 		// Horizontal
 		else if(g.Board[0][0] == g.Board[0][1] && g.Board[0][1] == g.Board[0][2] && g.Board[0][0] != 0)
 		{
-			g.winner = g.Board[0][0];
+			g.setWinner(g.Board[0][0]);
 		}
 		else if(g.Board[1][0] == g.Board[1][1] && g.Board[1][1] == g.Board[1][2] && g.Board[1][0] != 0)
 		{
-			g.winner = g.Board[1][0];
+			g.setWinner(g.Board[1][0]);
 		}
 		else if(g.Board[2][0] == g.Board[2][1] && g.Board[2][1] == g.Board[2][2] && g.Board[2][0] != 0)
 		{
-			g.winner = g.Board[2][0];
+			g.setWinner(g.Board[2][0]);
 		}
 	}
 	
@@ -59,13 +69,15 @@ public class Game
 		checkWinner(game);
 		while(game.winner == 0)
 		{
-			if(game.PlayerOne)
+			if(game.currentPlayer == 1)
 			{
-				
+				// Player One plays, color changed to red
+				game.setCurrentPlayer(2);
 			}
 			else
 			{
-				
+				// Player Two plays, color changed to blue
+				game.setCurrentPlayer(1);
 			}
 		}
 		System.out.println("Player" + game.winner + "won !");
